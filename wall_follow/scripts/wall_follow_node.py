000ee4451486a492c671dfe538a6f7fabe13adaf
naman-gupta99 <kungfunaman@gmail.com>
2023-11-21 00:07:42 +0530

    Naman | Comptletes Assignment 3

--- wall_follow/scripts/wall_follow_node.py
+++ wall_follow/scripts/wall_follow_node.py
@@ -5,6 +5,9 @@ from rclpy.node import Node
 import numpy as np
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
+from math import isfinite
+import time
+import atexit
 
 class WallFollow(Node):
     """ 
@@ -15,19 +18,21 @@ class WallFollow(Node):
 
         lidarscan_topic = '/scan'
         drive_topic = '/drive'
+        initialpose_topic = '/initialpose'
 
         self.lidar_sub = self.create_subscription(LaserScan, lidarscan_topic, self.scan_callback, 10)
-        self.drive_pub = self.create_publisher(AckermannDriveStamped, drive_topic, 0)
+        self.drive_pub = self.create_publisher(AckermannDriveStamped, drive_topic, 10)
+        self.initalpose_pub = self.create_publisher(AckermannDriveStamped, initialpose_topic, 10)
 
         self.kp = kp
         self.kd = kd
         self.ki = ki
-        self.lookahead_dist = 1
+        self.lookahead_dist = 1.0
 
-        self.integral = 0
-        self.prev_error = 0
-        self.error = 0
-        self.current_reading_time = self.get_clock().now()
+        self.integral = 0.0
+        self.prev_error = 0.0
+        self.error = 0.0
+        self.current_reading_time = time.time()
 
         self.angle_min = 0
         self.angle_increment = 0
@@ -44,9 +49,8 @@ class WallFollow(Node):
             range: range measurement in meters at the given angle
 
         """
-        index = int((angle - self.angle_min) / self.angle_increment)
-        return (range_data[index], self.angle_min + index * self.angle_increment)
-
+        index = int(np.floor((angle - self.angle_min) / self.angle_increment))
+        return range_data[index]
     def get_error(self, range_data: [float], dist: float):
         """
         Calculates the error to the wall. Follow the wall to the left (going counter clockwise in the Levine loop). You potentially will need to use get_range()
@@ -59,14 +63,24 @@ class WallFollow(Node):
             error: calculated error
         """
 
-        a, angle_a = self.get_range(range_data, np.pi/4)
-        b, angle_b = self.get_range(range_data, np.pi/2)
-        theta = angle_a - angle_b
+        angle_b = np.pi/2
+        angle_a = np.pi/4
+        while (not isfinite(self.get_range(range_data, angle_a))) or (angle_a == angle_b):
+            print('skipping a')
+            angle_a += 0.02
+
+        while (not isfinite(self.get_range(range_data, angle_b))) or (angle_a == angle_b):
+            print('skipping b')
+            angle_b += 0.02
+
+        a = self.get_range(range_data, angle_a)
+        b = self.get_range(range_data, angle_b)
+        theta = angle_b - angle_a
         
         alpha = np.arctan2((a * np.cos(theta) - b), (a * np.sin(theta)))
         curr_dist = b * np.cos(alpha)
         lookahead_dist = curr_dist + self.lookahead_dist * np.sin(alpha)
-        return -lookahead_dist - dist
+        return dist - lookahead_dist
 
     def get_velocity(self, angle: float):
         if angle < np.pi/18:
@@ -89,23 +103,24 @@ class WallFollow(Node):
         angle = 0.0
         
         previous_reading_time = self.current_reading_time
-        self.current_reading_time = self.get_clock().now()
-        dt = (self.current_reading_time - previous_reading_time).nanoseconds / 1e9
+        self.current_reading_time = time.time()
+        dt = (self.current_reading_time - previous_reading_time)
 
-        self.integral += error * dt
+        self.integral = self.prev_error * dt
         derivative = (error - self.prev_error) / dt
 
-        angle = self.kp * error + self.ki * self.integral + self.kd * derivative
+        angle = -(self.kp * error + self.ki * self.integral + self.kd * derivative)
 
         self.prev_error = error
 
-        velocity = self.get_velocity(angle)
+        if abs(error) < 0.1:
+            angle = 0.0
+
+        velocity = self.get_velocity(abs(angle))
 
         drive_msg = AckermannDriveStamped()
         drive_msg.drive.speed = velocity
         drive_msg.drive.steering_angle = angle
-
-        self.get_logger().info('angle: %f' % angle)
 
         self.drive_pub.publish(drive_msg)
         
@@ -124,10 +139,8 @@ class WallFollow(Node):
         self.angle_min = msg.angle_min
         self.angle_increment = msg.angle_increment
 
-        error = self.get_error(msg.ranges, 0.78)
-        self.get_logger().info('error: %f' % error)
-        self.pid_control(error) # TODO: actuate the car with PID
-        # exit()
+        error = self.get_error(msg.ranges, 1.0)
+        self.pid_control(error)
 
 
 def main(args=None):
